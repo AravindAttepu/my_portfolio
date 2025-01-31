79980d125c5c62fe33864df32dfc9b08c4c28625
tech_bachelor <88090152+AravindAttepu@users.noreply.github.com>
2025-01-31 20:07:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const project_tab= document.getElementById("project_tab");
 const projects_gate=  document.getElementById("projects_gate");
 const arrow= document.querySelector(".bx-chevron-down");
 
-projects_gate.addEventListener("click", function(){
+let button=true;
+let scaleUp = true;
+let intervalid=null;
 
-    project_tab.classList.toggle("hide")
-    arrow.classList.toggle("bx-rotate-180");
+projects_gate.addEventListener("click", function(){
+  button=!button;
+  project_tab.classList.toggle("hide")
+  arrow.classList.toggle("bx-rotate-180");
+  
+   
+
 
 });
